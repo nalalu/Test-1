4a53aa46ce9384423a2c23485564c8b6855c8ba8
jpy <jpy@woyouxinxi.com>
2016-02-17 17:36:31 +0800

    getIndex

--- ajax.php
+++ ajax.php
@@ -1,26 +1,24 @@
 <?php
-$api = $_GET['api'];
-$msg = $_POST['msg'];
-if(0){}
-elseif($api=='postmsg'){
+$api = getIndex($_GET, 'api');
+
+if(0){
+}
+elseif($api == 'postmsg'){
+  $msg = getIndex($_POST, 'msg');
   file_put_contents("test.txt", $msg);
-  $obj = ['code'=>1];
+  $obj = ['code' => 1];
   echo json_encode($obj);
 }
-elseif($api=='getmsg'){
+elseif($api == 'getmsg'){
   $msg = file_get_contents("test.txt");
-  $obj = ['code'=>1, 'msg'=>$msg];
+  $obj = ['code' => 1, 'msg' => $msg];
   echo json_encode($obj);
 }
-elseif($api=='postmsg'){}
-elseif($api=='postmsg'){}
-elseif($api=='postmsg'){}
-elseif($api=='postmsg'){}
-elseif($api=='postmsg'){}
-elseif($api=='postmsg'){}
-elseif($api=='postmsg'){}
 
 
+function getIndex($obj, $value, $default = ''){
+  return isset($obj[$value]) ? $obj[$value] : $default;
+}
 
 function showarr($obj){
   echo '<pre>';

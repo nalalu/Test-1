95e89e15602a98675900a82bfd36fae0904c6e1f
jpy <jpy@woyouxinxi.com>
2016-02-17 17:26:03 +0800

    save and echo code

--- ajax.php
+++ ajax.php
@@ -1,6 +1,12 @@
 <?php
-showarr($_GET);
-showarr($_POST);
+$api = $_GET['api'];
+$msg = $_POST['msg'];
+if($api=='postmsg'){
+  file_put_contents("test.txt", $msg);
+  $obj = ['code'=>1];
+  echo json_encode($obj);
+}
+
 function showarr($obj){
   echo '<pre>';
   print_r($obj);

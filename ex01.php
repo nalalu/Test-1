69fb0c31d36ac0b531b780e3801198edd5ac90ff
jpy <jpy@woyouxinxi.com>
2016-02-17 11:42:36 +0800

    no message

---
+++ ex01.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/2/17
+ * Time: 9:40
+ */
+$a=['A',2,3,4,5,6,7,8,9,10,'J','Q','K'];
+$b=['R','B','F','S'];
+$poke=['L','S'];
+
+for($i=0;$i<4;$i++){
+  for($j = 0; $j <13 ; $j++){
+    array_push($poke,$b[$i].$a[$j]);
+  }
+}
+
+showarray($poke);
+function showarray($obj){
+  echo '<pre>';
+  echo 'I LOVE YOU!';
+  print_r($obj);
+  echo '</pre>';
+}
